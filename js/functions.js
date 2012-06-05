abeb52e114ddf839e73dcc004a92fc49cc863d1c
Abhijeet Kinjawadekar <abhijeetgk@gmail.com>
2012-06-05 13:12:08 +0530

    Minor changes in formatting
    Signed-off-by: Abhijeet Kinjawadekar <abhijeetgk@gmail.com>

--- js/functions.js
+++ js/functions.js
@@ -1,29 +1,20 @@
-
-	
-		// When site loaded, load the Popupbox First
-		//loadPopupBox();
-	
-		$('#popupBoxClose').click( function() {			
-			unloadPopupBox();
-		});
-		
-		$('#container').click( function() {
-			unloadPopupBox();
-		});
-
-		function unloadPopupBox() {	// TO Unload the Popupbox
-			$('#popup_box').fadeOut("slow");
-			$("#container").css({ // this is just for style		
-				"opacity": "1"  
-			}); 
-		}	
-		
-		function loadPopupBox() {	// To Load the Popupbox
-		var offset1=$('#diaresult').offset();
-	
+$('#popupBoxClose').click( function() {			
+	unloadPopupBox();
+});
+$('#container').click( function() {
+	unloadPopupBox();
+});
+function unloadPopupBox() {	// TO Unload the Popupbox
+	$('#popup_box').fadeOut("slow");
+	$("#container").css({ // this is just for style		
+		"opacity": "1"  
+	}); 
+}	
+function loadPopupBox() {	// To Load the Popupbox
+	var offset1=$('#diaresult').offset();
 	$('#popup_box').css({ top: offset1.top, left: offset1.left });
 			$('#popup_box').fadeIn("slow");
 			$("#container").css({ // this is just for style
 				"opacity": "0.3"  
-			}); 		
-		}
+	}); 		
+}
